518dde4587e91e91e4e9736c2e8af6b9f064f49a
tish83 <tish83@gmail.com>
2022-04-02 13:06:21 +0200

    biblioteka csv

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #podstawowe podejście
 
-# with open("dane.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
+with open("dane.csv") as data_file:
+    data = data_file.readlines()
+    print(data)
 
 #wykorzystanie biblioteki csv
-# import csv
+import csv
 
-# with open("dane.csv") as data_file:
-#     data = csv.reader(data_file, delimiter=';')
-#     mails = []
-#     for row in data:
-#         if row[1]!="name":
-#             mails.append(row[2])
-#     print(mails)
-
-#wykorzystanie biblioteki pandas
-#należy ją najpierw zaimportować
+with open("dane.csv") as data_file:
+    data = csv.reader(data_file, delimiter=';')
+    mails = []
+    for row in data:
+        if row[1]!="name":
+            mails.append(row[2])
+    print(mails)
